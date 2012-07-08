f139a24a9f76b377d844fc171871121429d09199
Kyle Carter <kylcarte@umail.iu.edu>
2012-07-08 14:43:49 -0400

    moved ParseL01 out of GenGrammars directory

--- FrameworkHs/ParseL01.hs
+++ FrameworkHs/ParseL01.hs
@@ -1,78 +1,43 @@
 
 module FrameworkHs.ParseL01 where
 
-import FrameworkHs.GenGrammars.L01VerifyScheme
+import FrameworkHs.GenGrammars.L01
 import FrameworkHs.SExpReader.LispData
 import FrameworkHs.Prims
 import FrameworkHs.Helpers
 
 parseProg :: LispVal -> Exc Prog
-parseProg (List [(Symbol "letrec"),List bs,t]) =
-  do bs <- mapM parseTuple bs
-     t <- parseTail t
-     return (Letrec bs t)
-  where parseTuple :: LispVal -> Exc (Label,Tail)
-        parseTuple (List [l,List[(Symbol "lambda"),List [],t]]) =
-          do l <- parseLabel l
-             t <- parseTail t
-             return (l,t)
-        parseTuple e = failure ("Invalid tuple: " ++ show e)
+parseProg (List ((Symbol "begin"): ls)) =
+  do (ss,s) <- parseListWithFinal parseStatement parseStatement ls
+     return (Begin ss s)
 parseProg e = failure ("Invalid Prog: " ++ show e)
 
-parseTail :: LispVal -> Exc Tail
-parseTail (List [t]) =
-  do t <- parseTriv t
-     return (App t)
-parseTail (List ((Symbol "begin"):ls)) =
-  do (es,t) <- parseListWithFinal parseEffect parseTail ls
-     return (Begin es t)
-parseTail e = failure ("Invalid Tail: " ++ show e)
-
-parseEffect :: LispVal -> Exc Effect
-parseEffect (List [(Symbol "set!"),v,List[b,t1,t2]]) =
+parseStatement :: LispVal -> Exc Statement
+parseStatement (List [(Symbol "op-set!"),v1
+                     ,List [b,v2,x@(IntNumber _)]]) =
+  do v1 <- parseVar v1
+     b  <- parseBinop b
+     v2 <- parseVar v2
+     i <- parseInt32 x
+     return (OpSet v1 b v2 i)
+parseStatement (List [(Symbol "op-set!"),v1
+                     ,List [b,v2,x@(Symbol _)]]) =
+  do v1 <- parseVar v1
+     b  <- parseBinop b
+     v2 <- parseVar v2
+     v3 <- parseVar x
+     return (OpSet' v1 b v2 v3)
+parseStatement (List [(Symbol "set!"),v,x@(IntNumber _)]) =
   do v <- parseVar v
-     b <- parseBinop b
-     t1 <- parseTriv t1
-     t2 <- parseTriv t2
-     return (Set2 v b t1 t2)
-parseEffect (List [(Symbol "set!"),v,t]) =
+     i <- parseInt64 x
+     return (Set v i)
+parseStatement (List [(Symbol "set!"),v,x@(Symbol _)]) =
   do v <- parseVar v
-     t <- parseTriv t
-     return (Set1 v t)
-parseEffect e = failure ("Invalid Effect: " ++ show e)
-
-parseTriv :: LispVal -> Exc Triv
-parseTriv i@(IntNumber _) =
-  do i <- parseInt64 i
-     return (Integer i)
-parseTriv s@(Symbol _) =
-  catchExc (do v <- parseVar s
-               return (Var v))
-           (do l <- parseLabel s
-               return (Label l))
-parseTriv e = failure ("Invalid Triv: " ++ show e)
+     v' <- parseVar x
+     return (Set' v v')
+parseStatement e = failure ("Invalid Statement: " ++ show e)
 
 parseVar :: LispVal -> Exc Var
-parseVar x =
-  catchExc (do r <- parseReg x
-               return (Reg r))
-           (do f <- parseFVar x
-               return (FVar f))
-
--- (l-01
---   (start Prog)
---   (Prog
---    (letrec ((Label (lambda () Tail)) *) Tail))
---   (Tail
---    (app Triv)
---    (begin Effect * Tail))
---   (Effect
---    (set! Var Triv)
---    (set! Var (Binop Triv Triv)))
---   (Triv
---    Var
---    Integer
---    Label)
---   (Var
---    Reg
---    FVar))
+parseVar v =
+  do v <- parseReg v
+     return (Reg v)
