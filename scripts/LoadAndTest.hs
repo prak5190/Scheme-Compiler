35c5490f990a72f028c861fc86a70a4d585c5255
Kyle Carter <kylcarte@umail.iu.edu>
2012-07-19 18:27:57 -0400

    LoadAndTest only imports modules now

--- scripts/LoadAndTest.hs
+++ scripts/LoadAndTest.hs
@@ -1,8 +1,2 @@
 import FrameworkHs.Testing
 import FrameworkHs.Helpers (Option (Default, Option))
-
-type Valid = [TestResult]
-type Invalid = [TestResult]
-
-testDefault :: IO (Valid,Invalid)
-testDefault = testAll Default Default
