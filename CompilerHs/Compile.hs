fd97dac4f71566b4ec7b9b10b4d55e8b83f55068
Ryan Newton <rrnewton@gmail.com>
2013-01-23 12:38:39 -0500

    Edit Compile.hs to go along with upstream changes to driver.

--- CompilerHs/Compile.hs
+++ CompilerHs/Compile.hs
@@ -20,11 +20,6 @@ import CompilerHs.GenerateX86_64               (generateX86_64)
 
 import qualified Data.ByteString as B
 
-assemblyCmd :: String
-assemblyCmd = "cc"
-assemblyArgs :: [String]
-assemblyArgs = ["-m64","-o","t","t.s","Framework/runtime.c"]
-
 vfs = P423Pass { pass = verifyScheme
                , passName = "verifyScheme"
                , wrapperName = "verify-scheme/wrapper"
@@ -32,22 +27,8 @@ vfs = P423Pass { pass = verifyScheme
                }
 
 p423Compile :: P423Config -> LispVal -> IO String
-p423Compile c l = do
-  let p1 = runPassM c $ parseProg l
-  p <- runPass vfs c p1
-  assemble c $ generateX86_64 p
+p423Compile cfg l = runCompiler cfg $ do
+  p  <- liftPassM$ parseProg l
+  p  <- runPass vfs p
+  assemble$ generateX86_64 p
 
-------------------------------------------------------------
--- Helpers -------------------------------------------------
-
-assemble :: P423Config -> Gen -> IO String
-assemble c out =
-  case runGenM c out of
-    Left err -> error err
-    Right (_,bsout) -> do  
-      B.writeFile "t.s" bsout
-      (ec,_,e) <- readProcessWithExitCode assemblyCmd assemblyArgs ""
-      case ec of
-        ExitSuccess   -> do res <- readProcess "./t" [] ""
-                            return (chomp res)
-        ExitFailure i -> throw (AssemblyFailedException e)
