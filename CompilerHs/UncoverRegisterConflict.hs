51fb9526796e0908608da348edd15cfab431ebb3
Ryan Newton <rrnewton@gmail.com>
2013-01-27 08:28:44 -0500

    A bit of incremental hacking.  About to switch from Writer to State.

--- CompilerHs/UncoverRegisterConflict.hs
+++ CompilerHs/UncoverRegisterConflict.hs
@@ -1,4 +1,4 @@
-
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 
 module CompilerHs.UncoverRegisterConflict
   ( uncoverRegisterConflict
@@ -12,14 +12,31 @@ import FrameworkHs.Helpers
 import Control.Monad.State
 import Control.Monad
 
+import Control.Monad.Writer
+
+type Conflicts = [Int]
+
 -- Here we use a writer monad to accumulate a table of conflicts.
 
 uncoverRegisterConflict :: P423Config -> Prog -> T.Prog
-uncoverRegisterConflict _ (Letrec ls bd) = flip evalState "" $ do
-  let (labels,tails) = unzip ls
-  ts  <- mapM eTail tails
-  bd' <- eTail bd
-  return$ T.Letrec (zip labels ts) bd' 
+uncoverRegisterConflict _ (Letrec ls bd) = a
+  where
+    w :: Conflicts
+    (a,w) = runWriter $ do
+      let (labels,tails) = unzip ls
+      ts  <- mapM eBody tails
+      bd' <- eBody bd
+      return$ T.Letrec (zip labels ts) bd' 
 
-eTail = error "finish uncoverRegisterConflict"
+eBody :: Body -> Writer Conflicts T.Body
+eBody (Locals _uvs tl) =
+  eTail tl
+  error "finish uncoverRegisterConflict"
 
+
+eTail :: Tail -> Writer Conflicts T.Tail
+eTail tl =
+  case tl of
+    BeginT _ _ -> error "finish uncoverRegisterConflict"
+    IfT _ _ _ -> error "finish uncoverRegisterConflict" 
+    AppT  _ _ -> error "finish uncoverRegisterConflict" 
